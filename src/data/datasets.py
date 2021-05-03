52fbdf8e3db95cbdf7f07a33542e5c65efa0e235
Ilya Pakhalko <ipahalko@gmail.com>
2021-05-04 01:21:40 +0300

    added a template dataset class

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -1,3 +1,26 @@
 import os
+from glob import glob
+import re
 import sys
 import torch
+import torch.optim as optim
+from torch.utils.data import Dataset
+import pandas as pd
+import numpy as np
+
+
+class CadecDataset(Dataset):
+    def __init__(self, corpus_dir):
+        raise NotImplementedError
+
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(index):
+            index = index.tolist(index)
+
+        raise NotImplementedError
+
+
+    def __len__(self):
+
+        raise NotImplementedError
