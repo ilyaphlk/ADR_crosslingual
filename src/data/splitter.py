27d8f8f2db24f8236719ade44dd1bbccadfbca8f
Ilya Pakhalko <ipahalko@gmail.com>
2021-05-12 19:07:35 +0300

    turned split functions into a class

--- src/data/splitter.py
+++ src/data/splitter.py
@@ -4,63 +4,80 @@ import numpy as np
 from shutil import copy2
 
 
-def is_consistent_set(txt_files, ann_files):
-    for txt, ann in zip(txt_files, ann_files):
-        if txt.split('/')[-1][:-4] != ann.split('/')[-1][:-4]:
-            return False
-    return True
+class CadecSplitter:
+    def __init__(self, corpus_dir, train_share, test_share, dev_share=0., name_prefix="", override=False, shuffle=True):
+        '''
+            corpus_dir - path to folder containing CADEC corpus. Must contain "original" and "text" folders
+            <fold>_share - expected sampling share of a fold
+            name_prefix - prefix for naming resulting folds directories
+            override - whether or not to rewrite the existing split
+        '''
+        self.corpus_dir = corpus_dir
+        self.train_share = train_share
+        self.test_share = test_share
+        self.dev_share = dev_share
+        self.name_prefix = name_prefix
+        self.override = override
+        self.split_successful = False
 
 
-def make_fold(txt_files, ann_files, fold_share, idx, left, fold_path, debug):
-    # todo make option for deleting folds
-    fold_len = int(fold_share * idx.shape[0])
-    fold_idx = idx[left:left+fold_len]
-    if debug:
-        fold_path += '_debug'
+    def split():
 
-    try: 
+        # todo: check split for existence
+        self.train_path = os.path.join(corpus_dir, name_prefix+"_train")
+        self.test_path = os.path.join(corpus_dir, name_prefix+"_test")
+        self.dev_path = os.path.join(corpus_dir, name_prefix+"_dev")
+
+        if (any(os.path.exists(train_path),
+                os.path.exists(test_path),
+                os.path.exists(dev_path))):
+            # TODO implement override logic
+            pass
+
+        txt_files_pattern = os.path.join(corpus_dir, 'text', '*.txt')
+        ann_files_pattern = os.path.join(corpus_dir, 'original','*.ann')
+
+        self.txt_files = sorted([txt_file for txt_file in glob(txt_files_pattern)])
+        self.ann_files = sorted([ann_file for ann_file in glob(ann_files_pattern)])
+
+
+        # TODO: find consistent pairs and use them
+        if not self.is_consistent_set():
+            raise Exception("found mismatched pairs of txt/ann files.")
+
+
+        self.idx = np.arange(txt_files.shape[0])
+        if self.shuffle:
+            np.random.shuffle(self.idx)
+
+
+        train_len = make_fold(self.train_share, 0, self.train_path)
+        test_len = make_fold(self.test_share, train_len, self.test_path)
+        dev_len = make_fold(self.dev_share, train_len + test_len, self.dev_path)
+
+        return train_len, dev_len, test_len
+
+
+    def is_consistent_set(self):
+        for txt, ann in zip(self.txt_files, self.ann_files):
+            if txt.split('/')[-1][:-4] != ann.split('/')[-1][:-4]:
+                return False
+        return True
+
+
+
+    def make_fold(self, fold_share, start_idx, fold_path):
+        # todo make option for deleting folds
+        fold_len = int(fold_share * self.idx.shape[0])
+        fold_idx = self.idx[start_idx:start_idx+fold_len]
+
         os.mkdir(fold_path)
-    except OSError as error: 
-        print("Skipping mkdir;", error)
 
-    for txt_file, ann_file in zip(txt_files[fold_idx], ann_files[fold_idx]):
-        copy2(txt_file, fold_path)
-        copy2(ann_file, fold_path)
+        for txt_file, ann_file in zip(self.txt_files[fold_idx], self.ann_files[fold_idx]):
+            copy2(txt_file, fold_path)
+            copy2(ann_file, fold_path)
 
-
-
-    return fold_len
-
-
-def splitter(corpus_dir, train_share, dev_share, test_share=0., debug=False):
-    txt_files_pattern = os.path.join(corpus_dir, 'text', '*.txt')
-    ann_files_pattern = os.path.join(corpus_dir, 'original','*.ann')
-
-    txt_files = sorted([txt_file for txt_file in glob(txt_files_pattern)])
-    ann_files = sorted([ann_file for ann_file in glob(ann_files_pattern)])
-
-    # TODO: check consistency - we need only matched pairs of files
-    if not is_consistent_set(txt_files, ann_files):
-        raise Exception("found mismatched pairs of txt/ann files.")
-
-
-    # Now everything is consistent
-    txt_files = np.array(txt_files)
-    ann_files = np.array(ann_files)
-
-    idx = np.arange(txt_files.shape[0])
-    np.random.shuffle(idx)
-
-    train_len = make_fold(txt_files, ann_files, train_share,
-        idx, 0, os.path.join(corpus_dir, "train"), debug)
-
-    dev_len = make_fold(txt_files, ann_files, dev_share,
-        idx, train_len, os.path.join(corpus_dir, "dev"), debug)
-
-    test_len = make_fold(txt_files, ann_files, test_share,
-        idx, train_len+dev_len, os.path.join(corpus_dir, "test"), debug)
-
-    return train_len, dev_len, test_len
+        return fold_len
 
 
 if __name__ == "__main__":
