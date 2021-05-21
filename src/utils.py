d58bd61174fcb9ca6ef1a12b17559273f62dfddc
Ilya Pakhalko <ipahalko@gmail.com>
2021-05-21 15:10:11 +0300

    added compute_metrics

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import torch
 from torch.nn.utils.rnn import pad_sequence
 import time
 import datetime
+from seqeval.metrics import f1_score
+from seqeval.metrics import accuracy_score
 
 
 def collate_dicts(samples, pad_id=0):
@@ -47,3 +49,22 @@ def unpack(batches, original_lens_batches):
         for row, len in zip(batch.tolist(), original_lens):
             unpacked.append(row[:len])
     return unpacked
+
+
+def compute_metrics(labels, preds, original_lens, int2label):
+    '''
+        labels, result, original_lens - each is a list of batched tensors of shape (batch_len, seq_len)
+    '''
+    # we are only intrested in metrics for non-pad tokens
+    # so we must filter them out first
+
+    labels = [list(map(lambda x: int2label[x], doc)) for doc in unpack(labels, original_lens)]
+    preds = [list(map(lambda x: int2label[x], doc)) for doc in unpack(preds, original_lens)]
+
+    f1 = f1_score(labels, preds)
+    acc = accuracy_score(labels, preds)
+
+    return {
+        'accuracy': acc,
+        'f1': f1,
+    }
